44bcd1347083f23bcc0c5dfab35d2a407a901e90
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-03-07 14:44:48 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,61 @@
+const zucchine = [
+  {
+    varieta: romanesca,
+    peso: 10,
+    lunghezza: 15,
+  },
+
+  {
+    varieta: nera,
+    peso: 9,
+    lunghezza: 20,
+  },
+
+  {
+    varieta: ortolana,
+    peso: 8,
+    lunghezza: 10,
+  }, 
+
+  {
+    varieta: siciliana,
+    peso: 12,
+    lunghezza: 27,
+  },
+
+  {
+    varieta: striata,
+    peso: 11,
+    lunghezza: 18,
+  },
+
+  {
+    varieta: bianca,
+    peso: 15,
+    lunghezza: 7,
+  },
+
+  {
+    varieta: tonda,
+    peso: 13,
+    lunghezza: 5,
+  },
+
+  {
+    varieta: trombetta,
+    peso: 20,
+    lunghezza: 28,
+  },
+
+  {
+    varieta: gialla,
+    peso: 12,
+    lunghezza: 18,
+  },
+
+  {
+    varieta: crookneck,
+    peso: 9,
+    lunghezza: 20,
+  },
+]
