72bca8482abf549e2e3078f404e372f3b6da3883
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-03-07 15:24:54 +0100

    primo snack

--- js/script.js
+++ js/script.js
@@ -1,61 +1,66 @@
 const zucchine = [
   {
-    varieta: romanesca,
+    varieta: 'romanesca',
     peso: 10,
     lunghezza: 15,
   },
 
   {
-    varieta: nera,
+    varieta: 'nera',
     peso: 9,
     lunghezza: 20,
   },
 
   {
-    varieta: ortolana,
+    varieta: 'ortolana',
     peso: 8,
     lunghezza: 10,
   }, 
 
   {
-    varieta: siciliana,
+    varieta: 'siciliana',
     peso: 12,
     lunghezza: 27,
   },
 
   {
-    varieta: striata,
+    varieta: 'striata',
     peso: 11,
     lunghezza: 18,
   },
 
   {
-    varieta: bianca,
+    varieta: 'bianca',
     peso: 15,
     lunghezza: 7,
   },
 
   {
-    varieta: tonda,
+    varieta: 'tonda',
     peso: 13,
     lunghezza: 5,
   },
 
   {
-    varieta: trombetta,
+    varieta: 'trombetta',
     peso: 20,
     lunghezza: 28,
   },
 
   {
-    varieta: gialla,
+    varieta: 'gialla',
     peso: 12,
     lunghezza: 18,
   },
 
   {
-    varieta: crookneck,
+    varieta: 'crookneck',
     peso: 9,
     lunghezza: 20,
-  },
+  }
 ]
+
+// Destrutturo l'array
+const [peso] = zucchine;
+console.log(zucchine).peso;
+
